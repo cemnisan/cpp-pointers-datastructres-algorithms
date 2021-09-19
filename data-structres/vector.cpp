bc720d6c0d915fa5d6a67fd18af62e70500d643e
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-09-20 00:11:44 +0300

    begin-end, at & operator, and insert-1

--- data-structres/vector.cpp
+++ data-structres/vector.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+namespace ns2 {
+    template <class T>
+    void swap(T& x, T& y){
+        T temp = x;
+        x = y;
+        y = temp;
+    }
+}
+
 namespace ns{
     
     template<class T>
@@ -33,10 +42,16 @@ namespace ns{
                     for(int i=0; i < size(); i++){
                         tmp[i] = data[i];
                     }
-
                     delete[] data;
                     data = tmp;
                 }
+            }
+
+            void reset(int index = 0, int cap = 1){
+                delete [] data;
+                data = NULL;
+                this->index = index;
+                this->cap = cap;
             }
 
         public:
@@ -50,14 +65,12 @@ namespace ns{
             void push_back(const T& value){
                 growth();
                 data[index++] = value;
-
             }
 
             void pop_back(){
                 if(isEmpty()){
                     throw "Error : Vector is empty.";
                 }
-        
                 index--;
                 shrink();
             }
@@ -70,7 +83,6 @@ namespace ns{
                 if(isEmpty()){
                     throw "Error : Vector is empty.";
                 }
-
                 return data[0];
             }
 
@@ -78,19 +90,99 @@ namespace ns{
                 if(isEmpty()){
                     throw "Error : Vector is empty.";
                 }
-
                 return data[index - 1];
             }
+
+            T* begin()const{
+                return data;
+            }
+
+            T* end()const{
+                return data + size();
+            }
+
+            T& at(int indices){
+                if(indices >= 0 && indices < size()){
+                    return data[indices];
+                }
+                throw "Error : Vector indices overflow.";
+            }
+
+            T& operator[](int indices){
+
+                return at(indices);
+            }
+
+            void clear(){
+                reset();
+                data = new T[cap];
+            }
+
+            void insert(const T* pos, const T& value){
+                if (begin() <= pos && pos <= end()){
+                    int index = pos - begin();
+                    push_back(value);
+                    for(int i = size() - 1; i > index; i--){
+                        ns2::swap(data[i],data[i - 1]);
+                        /*Example:
+                            1 2 3
+                            1 2 3 0
+                            1 2 0 3
+                            1 0 2 3
+                            0 1 2 3 --> Complete.*/
+                    }
+                }
+                else
+                    throw "Error : Vector instert";
+            }
+
     };   
 }
 
 int main(){
     ns::vector<int> obj;
-
+    
     try{
-       
-       obj.pop_back();
+        /*obj.push_back(34);
+        obj.push_back(32);
+        obj.push_back(35);
+        obj.push_back(44);
+        obj.push_back(55);
 
+        cout << "size: " << obj.size() << endl;
+        cout << "capacity: " << obj.capacity() << endl;
+
+        int* begin = obj.begin();
+        int* end = obj.end();
+
+        cout << "begin : " << begin << endl;
+        cout << "end : " << end << endl;
+
+        while(begin != end){
+            cout << *begin << endl;
+            begin++;
+        }
+
+        cout << "1. indices: " << obj[1] << endl;
+
+        obj.clear();
+
+        cout << "size: " << obj.size() << endl;
+        cout << "capacity: " << obj.capacity() << endl;*/
+
+        obj.push_back(1);
+        obj.push_back(3);
+        obj.push_back(5);
+        obj.insert(&obj[1],2);
+        int *begin = obj.begin();
+        int *end = obj.end();
+
+        while(begin != end){
+            cout << *begin << " ";
+            begin++;
+        }
+
+   
     }catch(const char* exception){
         cout << exception;
     }
