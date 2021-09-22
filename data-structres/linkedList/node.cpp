109a25cab5b986793d845e6d685506c321dac381
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-09-22 22:05:17 +0300

    push_back method added

--- data-structres/linkedList/node.cpp
+++ data-structres/linkedList/node.cpp
@@ -14,18 +14,41 @@ class list{
     Node* root;
     public:
         list(): root(NULL){}
+
         Node* begin()const{ return root; }
+
         Node* end()const{ return NULL; }
+
         // bool isEmpty()const{return root == NULL; } or
         bool isEmpty()const { 
             return begin() == end(); 
+        }
+
+        void push_back(const int& value){
+            if(isEmpty()){
+                root = new Node(value);
+
+            }
+            else{
+                Node* tmp = begin();
+
+                while(tmp->next != end()){
+                    tmp = tmp->next;
+                }
+                tmp->next = new Node(value);
+            
+            }
         }
 };
 
 int main(){
 
     list l;
-    cout << l.isEmpty();
+    
+    cout << l.isEmpty() << endl;
+
+    l.push_back(6);
+    cout << l.isEmpty() << endl;
 
 
     return 0;
