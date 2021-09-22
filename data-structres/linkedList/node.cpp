c83ba1e06a3665d2277f24de7b4a56c4e3f051fd
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-09-22 21:43:35 +0300

    methods:begin,end,isEmpty

--- data-structres/linkedList/node.cpp
+++ data-structres/linkedList/node.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 using namespace std;
 
-
-template <class T>
+// Constructor Node
 class Node{
-
     public:
-        Node(const T& data = T(),Node<T>* next=NULL): data(data), next(next) { }
-        T data;
-        Node<T>* next;
+        Node(const int& data = 0,Node* next=NULL): data(data), next(next) { }
+        int data;
+        Node* next;
 };
 
+// Constructor List
+class list{
+    Node* root;
+    public:
+        list(): root(NULL){}
+        Node* begin()const{ return root; }
+        Node* end()const{ return NULL; }
+        // bool isEmpty()const{return root == NULL; } or
+        bool isEmpty()const { 
+            return begin() == end(); 
+        }
+};
 
 int main(){
 
-    Node<int>* n1 = new Node<int>(3);
-    //cout << n1->data << endl;
+    list l;
+    cout << l.isEmpty();
 
-    Node<int>* n2 = new Node<int>(5);
-    n1->next = n2;
-    //cout << n2-> data << endl;
-
-    Node<int>* n3 = new Node<int>(10);
-    n2->next = n3;
-    //cout << n3->data << endl;
-
-    /*cout << n1->data << " ";
-    cout << n1->next->data << " ";
-    cout << n1->next->next->data;*/
-
-    Node<int>* tmp = n1;
-
-    while(tmp != NULL){
-        cout << tmp->data << " ";
-        tmp = tmp->next;
-    }
- 
 
     return 0;
 }
